3ddaa3b19cf65e30e9320524d200913b194e61b1
Katherine Mann <kkjayne@gmail.com>
2016-11-05 05:45:34 +0800

    progress towards getting it working

--- ict286/www/js/index.js
+++ ict286/www/js/index.js
@@ -1,29 +1,9 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
- 
-var slideIndex = 0;
-var quoteIndex = 0;
- 
 var app = {
     // Application Constructor
     initialize: function() {
         this.bindEvents();
+		this.slideIndex = 0;
+		this.quoteIndex = 0;
     },
     // Bind Event Listeners
     //
@@ -37,33 +17,22 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-		app.loaded();
+		app.carousel();
+		app.showBookOfWk(11111111);
+		app.showRecentBooks();
+		app.displayQuotes();
     },
-	loaded: function() {
-		banner.carousel();
-		homepage.showBookOfWk(11111111);
-		homepage.showRecentBooks();
-		homepage.displayQuotes();
-	}
-};
-
-app.initialize();
-
-//global array for cart
-var Cart = [];
-
-//for banner
-var banner = {
 	carousel: function () {
 		var i;
 		var x = document.getElementsByClassName("coInfo");
 		for (i = 0; i < x.length; i++) {
 		  x[i].style.display = "none"; 
+		  console.log("checking elements " + x[i]);
 		}
-		slideIndex++;
-		if (slideIndex > x.length) {slideIndex = 1} 
-		x[slideIndex-1].style.display = "block"; 
-		setTimeout(carousel, 3000); // Change image every 3 seconds
+		this.slideIndex++;
+		if (this.slideIndex > x.length) {this.slideIndex = 1} 
+		x[this.slideIndex-1].style.display = "block"; 
+		setTimeout(this.carousel, 3000); // Change image every 3 seconds
 	},
 
 	//to make div's visibile/hidden
@@ -100,6 +69,16 @@ var banner = {
 		document.getElementById(oldPgId).removeAttribute("class");
 		curPg.setAttribute("class", "active");
 	}
+};
+
+app.initialize();
+
+//global array for cart
+var Cart = [];
+
+//for banner
+var banner = {
+	
 };
 
 //***** start functions for home page *****
